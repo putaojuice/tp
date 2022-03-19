1df055d851ddf6af9730be847408b8823f16a489
putaojuice <33406416+putaojuice@users.noreply.github.com>
2022-03-20 02:04:34 +0800

    Update on AddTaskCommandParser
    - Add DateTimeFormatter in AddTaskParser
    - Add tests for the usage of DateTimeFormatter
    - Refactor AddTaskCommandParser

--- src/test/java/seedu/address/logic/parser/AddTaskCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddTaskCommandParserTest.java
@@ -13,8 +13,8 @@ public class AddTaskCommandParserTest {
 
     @Test
     public void parse_allFieldPresent_success() {
-        assertParseSuccess(parser, "addt d/description t/2022",
-                new AddTaskCommand("description", "2022"));
+        assertParseSuccess(parser, "addt d/description t/01/01/2022",
+                new AddTaskCommand("description", "01/01/2022"));
     }
 
     @Test
@@ -32,7 +32,7 @@ public class AddTaskCommandParserTest {
     @Test
     public void parse_multipleDeadline_failure() {
         String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE);
-        assertParseFailure(parser, "addt d/abc t/2022 t/2022", expectedMessage);
+        assertParseFailure(parser, "addt d/abc t/01/01/2022 t/01/01/2022", expectedMessage);
     }
 
     @Test
@@ -42,13 +42,43 @@ public class AddTaskCommandParserTest {
         // has description prefix
         assertParseFailure(parser, "addt d/", expectedMessage);
 
+        // has description prefix and deadline prefix
+        assertParseFailure(parser, "addt d/ t/", expectedMessage);
+
         // no description prefix
         assertParseFailure(parser, "addt", expectedMessage);
     }
 
     @Test
-    public void parse_invalidDeadlineFormat_failure() {
-        // TODO to be added when implementing DateTimeFormatter
+    public void parse_deadlineFormat_success() {
+        assertParseSuccess(parser, "addt d/description t/01/01/2022",
+                new AddTaskCommand("description", "01/01/2022"));
     }
 
+    @Test
+    public void parse_deadlineFormat_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE);
+
+        // no "/" between date, month, year
+        assertParseFailure(parser, "addt d/description t/01012022", expectedMessage);
+
+        // uses "-" between date, month, year
+        assertParseFailure(parser, "addt d/description t/01-01-2022", expectedMessage);
+    }
+
+    @Test
+    public void parse_deadlineFormatUseAlphabets_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, "addt d/description t/monday", expectedMessage);
+        assertParseFailure(parser, "addt d/description t/o1/o1/2o22", expectedMessage);
+    }
+
+    @Test
+    public void parse_deadlineFormatUseSymbols_failure() {
+        String expectedMessage = String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddTaskCommand.MESSAGE_USAGE);
+
+        assertParseFailure(parser, "addt d/description t/!@#$%^&*(){}[]|~`", expectedMessage);
+        assertParseFailure(parser, "addt d/description t/*10!2()22", expectedMessage);
+    }
 }
