3b1e4b8747bd4f927338b4cdc00e979cae761b23
castryl <e0556036@u.nus.edu>
2022-03-21 17:11:03 +0800

    Add test coverage for view task command

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -167,7 +167,13 @@ public class AddCommandTest {
 
         @Override
         public String findTask(String input) {
-            throw new AssertionError("This method should not be called."); }
+            throw new AssertionError("This method should not be called.");
+        }
+
+        @Override
+        public String viewTask() {
+            throw new AssertionError("This method should not be called.");
+        }
     }
 
     /**
